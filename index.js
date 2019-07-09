1fbaa011caab18231bf94d15b3ad9dcac3cb5e01
nbarikipoulos <nicolas.barriquand@outlook.fr>
2019-07-09 17:44:38 +0200

    Re-export poppy core factories and objects.

---
+++ index.js
@@ -0,0 +1,91 @@
+/*! Copyright (c) 2018-2019 Nicolas Barriquand <nicolas.barriquand@outlook.fr>. MIT licensed. */
+
+/**
+ * 
+ * This module is a simple wrapper of the poppy-robot-core module in order to:
+ * 
+ *  - Automatically manage a set of common flags dedicated to robot connections via node cli,
+ *  - Read poppy rc file created neither by hand or using the cli tool.
+ * 
+ * The flags in order to set the connection to poppy:
+ * 
+ * option | desc | value | default
+ * --- | --- | --- | --- 
+ * -i/--ip | Set the Poppy IP/hostname | string | poppy.local
+ * -p/--http-port | Set the http server port on Poppy | integer | 8080
+ * -P/--snap-port | Set the snap server port on Poppy | integer | 6969
+ * 
+ * Note it re-exports all the exported features of the poppy-robot-core module.
+ * 
+ * @module poppy-robot-cli
+ * @typicalname P
+ * @version 3.0.0
+ * @see {@link https://github.com/nbarikipoulos/poppy-robot-core.git}
+ */
+
+'use strict'
+
+const yargs = require('yargs');
+
+const cliOptions = require('./bin/cli-options'),
+    OptionHelper = cliOptions.OptionHelper,
+    getPoppyConfiguration = cliOptions.getPoppyConfiguration
+;
+
+const core = require('poppy-robot-core'),
+    Script = core.Script,
+    Poppy = core.Poppy,
+    ExtMotorRequest = core.ExtMotorRequest,
+    RawMotorRequest = core.ExtMotorRequest,
+    PoppyRequestHandler = core.PoppyRequestHandler
+;
+
+//////////////////////////////////
+// Automatically add CLI options for 
+// Poppy configuration to any script
+//////////////////////////////////
+
+yargs
+    .locale('en')
+    .alias('h','help')
+    .help('h')
+;
+
+let optionHelper = new OptionHelper();
+optionHelper.addPoppyConfigurationOptions(yargs);
+
+yargs
+    .wrap(yargs.terminalWidth())
+    .argv
+;
+
+//////////////////////////////////
+// Main object factories
+//////////////////////////////////
+
+let createPoppy = (options) => {
+
+    // First let's obtain the configuration
+    let config = Object.assign( {},
+        getPoppyConfiguration(yargs.argv), // read from .poppyrc and CLI
+        options // from arguments
+    );
+
+    return core.createPoppy(config);
+}
+
+//////////////////////////////////
+//////////////////////////////////
+// Public API
+//////////////////////////////////
+//////////////////////////////////
+
+module.exports = {
+    createScript,
+    createScript: core.createPoppy,
+    Script,
+    Poppy,
+    ExtMotorRequest,
+    RawMotorRequest,
+    PoppyRequestHandler
+};
